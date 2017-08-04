7664ba997dd18d0d2aa704168d2d50e1190d8e1e
Victor Arnaud <victorhad@gmail.com>
2017-08-03 23:18:36 -0300

    Remove unused img and refactoring core application

--- pet/core/forms.py
+++ pet/core/forms.py
@@ -4,16 +4,22 @@ from .email import send_email_template
 
 
 class ContactForm(forms.Form):
-  name = forms.CharField(label='Nome')
-  email = forms.EmailField(label='Email')
-  message = forms.CharField(label='Mensagem', widget=forms.Textarea)
+    name = forms.CharField(label='Nome')
+    email = forms.EmailField(label='Email')
+    message = forms.CharField(label='Mensagem', widget=forms.Textarea)
 
-  def send_message(self):
-    template = 'core/contact_email.html'
-    subject = 'PET: E-mail do [%s]' % self.cleaned_data['name']
-    context = {
-      'name': self.cleaned_data['name'],
-      'email': self.cleaned_data['email'],
-      'message': self.cleaned_data['message']
-    }
-    send_email_template(subject, template, context, [settings.CONTACT_EMAIL], from_email=context['email'])
+    def send_message(self):
+        template = 'core/contact_email.html'
+        subject = 'PET: E-mail do [%s]' % self.cleaned_data['name']
+        context = {
+            'name': self.cleaned_data['name'],
+            'email': self.cleaned_data['email'],
+            'message': self.cleaned_data['message']
+        }
+        send_email_template(
+            subject,
+            template,
+            context,
+            [settings.CONTACT_EMAIL],
+            from_email=context['email']
+        )
