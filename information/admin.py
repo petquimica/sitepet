a0eb4e7fda87dc8fb7078dd7443e6e644a1b68e5
Victor Arnaud <victorhad@gmail.com>
2017-08-03 23:34:20 -0300

    Finish information

--- information/admin.py
+++ information/admin.py
@@ -3,11 +3,11 @@ from .models import Information
 
 
 class InformationAdmin(admin.ModelAdmin):
-  fields = ['title', 'slug', 'content', 'tags', 'link']
-  list_display = ['title', 'slug', 'created_at']
-  search_fields = ['title', 'content']
-  list_filter = ['created_at']
-  prepopulated_fields = {'slug': ('title',)}
+    fields = ['title', 'slug', 'content', 'tags', 'link']
+    list_display = ['title', 'slug', 'created_at']
+    search_fields = ['title', 'content']
+    list_filter = ['created_at']
+    prepopulated_fields = {'slug': ('title',)}
 
 
 admin.site.register(Information, InformationAdmin)
--- information/models.py
+++ information/models.py
@@ -1,25 +1,28 @@
 from django.db import models
 from taggit.managers import TaggableManager
-from django.core.urlresolvers import reverse
+
 
 class Information(models.Model):
-  title = models.CharField('Título', max_length=100)
-  slug = models.SlugField('Identificador', max_length=100)
-  content = models.TextField('Conteúdo', max_length=100000)
-  tags = TaggableManager(blank=True)
-  image = models.ImageField(upload_to='documents/information', verbose_name="Imagem", blank=True, null=True)
-  link = models.URLField('Google Driver', help_text='Link para o Google Driver', blank=True)
-  created_at = models.DateTimeField('Criado em', auto_now_add=True)
-  updated_at = models.DateTimeField('Modificado em', auto_now=True)
+    title = models.CharField('Título', max_length=100)
+    slug = models.SlugField('Identificador', max_length=100)
+    content = models.TextField('Conteúdo', max_length=100000)
+    tags = TaggableManager(blank=True)
+    link = models.URLField(
+        'Google Driver',
+        help_text='Link para o Google Driver',
+        blank=True
+    )
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    updated_at = models.DateTimeField('Modificado em', auto_now=True)
 
-  def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
 
-  @models.permalink
-  def get_absolute_url(self):
-    return ('information:details', (), {'slug': self.slug})
+    @models.permalink
+    def get_absolute_url(self):
+        return ('information:details', (), {'slug': self.slug})
 
-  class Meta:
-    verbose_name = 'Informação'
-    verbose_name_plural = 'Informações'
-    ordering = ['-created_at']
+    class Meta:
+        verbose_name = 'Informação'
+        verbose_name_plural = 'Informações'
+        ordering = ['-created_at']
