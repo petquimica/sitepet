ded8be3183df8c4cef7b18bcd60b86fdd7fb1553
Victor Arnaud <victorhad@gmail.com>
2017-08-03 23:02:20 -0300

    Remove 3 time members

--- team/views.py
+++ team/views.py
@@ -2,7 +2,7 @@ from django.views.generic import TemplateView
 
 
 class TeamView(TemplateView):
-    template_name = 'team/team_static.html'
+    template_name = 'team/team.html'
 
 
 class HistoricView(TemplateView):
